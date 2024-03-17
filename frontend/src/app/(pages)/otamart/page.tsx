f03c62f73d201dc7f53eeb505e722be26e631251
Reinaldo Guedes <rguedes.dev@gmail.com>
2024-03-17 05:59:39 -0300

    Melhorias na feature Checkout/2

--- frontend/src/app/(pages)/otamart/page.tsx
+++ frontend/src/app/(pages)/otamart/page.tsx
@@ -43,21 +43,21 @@ function OtamartPage() {
 	}, []);
 
 	return (
-		<section className="grid grid-cols-6 md:grid-cols-8 grid-rows-1 gap-4 mx-4">
+		<section className="grid grid-cols-6 md:grid-cols-8 grid-rows-1 gap-4 min-h-screen mx-4">
 			<div className="flex flex-col items-center justify-center bg-yellow-500 col-start-2 col-span-4 md:col-start-2 md:col-span-6">
 				<div className="divider text-xl md:text-2xl font-semibold">
 					Categorias
 				</div>
 				<div className="flex flex-row justify-center gap-4 my-3">
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<GiEvilBook className="mb-1" size={40} />
 							<span className="text-xs">Impressos</span>
 						</div>
 					</div>
 
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<IoGameControllerOutline
 								className="mb-1"
 								size={40}
@@ -67,49 +67,49 @@ function OtamartPage() {
 					</div>
 
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<GiBattleMech className="mb-1" size={40} />
 							<span className="text-xs">Figures</span>
 						</div>
 					</div>
 
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<LuDisc3 className="mb-1" size={40} />
 							<span className="text-xs">CDs/DVDs</span>
 						</div>
 					</div>
 
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<PiTShirt className="mb-1" size={40} />
 							<span className="text-xs">Roupas</span>
 						</div>
 					</div>
 
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<BsSmartwatch className="mb-1" size={40} />
 							<span className="text-xs">Acessórios</span>
 						</div>
 					</div>
 
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<TbCards className="mb-1" size={40} />
 							<span className="text-xs">TCG</span>
 						</div>
 					</div>
 
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<RiPencilRuler2Line className="mb-1" size={40} />
 							<span className="text-xs">Papelaria</span>
 						</div>
 					</div>
 
 					<div className="flex items-center">
-						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 cursor-pointer">
+						<div className="flex flex-col justify-center items-center w-20 h-20 bg-primary rounded-md shadow-lg transition-all ease-in hover:scale-110 active:scale-[.97] cursor-pointer">
 							<GiProtectionGlasses className="mb-1" size={40} />
 							<span className="text-xs">Óculos</span>
 						</div>
@@ -124,7 +124,7 @@ function OtamartPage() {
 				</div>
 			</div>
 
-			<div className="bg-yellow-500 col-start-2 col-span-4 md:col-start-2 md:col-span-6 mb-8 min-h-screen">
+			<div className="bg-yellow-500 col-start-2 col-span-4 md:col-start-2 md:col-span-6 mb-8">
 				<div className="divider text-center text-xl md:text-2xl font-semibold mb-8">
 					Produtos em Destaque
 				</div>
