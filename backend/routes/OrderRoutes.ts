faaf4e36e08e91d77d618c19a0b410d5119a3940
Reinaldo Guedes <rguedes.dev@gmail.com>
2025-05-04 07:12:01 -0300

    Pequenos ajustes ao longo da aplicação.

--- backend/routes/OrderRoutes.ts
+++ backend/routes/OrderRoutes.ts
@@ -10,46 +10,52 @@ import { imageUpload } from "../helpers/image-upload.js";
 router.get("/partner-orders", verifyToken, OrderController.getAllPartnerOrders);
 
 router.get(
-	"/partner-orders/:id",
-	verifyToken,
-	OrderController.getPartnerOrderByID
+  "/partner-orders/:id",
+  verifyToken,
+  OrderController.getPartnerOrderByID
 );
 
 router.get(
-	"/customer-orders",
-	verifyToken,
-	OrderController.getAllCustomerOrders
+  "/customer-orders",
+  verifyToken,
+  OrderController.getAllCustomerOrders
 );
 router.get(
-	"/customer-orders/:id",
-	verifyToken,
-	OrderController.getCustomerOrderByID
+  "/customer-orders/:id",
+  verifyToken,
+  OrderController.getCustomerOrderByID
 );
 
 router.patch(
-	"/customer-receiptorder/:id",
-	verifyToken,
-	OrderController.confirmReceiptCustomerOrder
+  "/customer-receiptorder/:id",
+  verifyToken,
+  OrderController.confirmReceiptCustomerOrder
 );
 
 router.patch("/mark-packed/:id", verifyToken, OrderController.markPacked);
 
 router.patch(
-	"/mark-delivered/:id",
-	verifyToken,
-	OrderController.markOrderDelivered
+  "/mark-delivered/:id",
+  verifyToken,
+  OrderController.markOrderDelivered
 );
 
 router.patch(
-	"/update-trackingcode/:id",
-	verifyToken,
-	OrderController.updateTrackingCode
+  "/mark-not-delivered/:id",
+  verifyToken,
+  OrderController.markOrderNotDelivered
+);
+
+router.patch(
+  "/update-trackingcode/:id",
+  verifyToken,
+  OrderController.updateTrackingCode
 );
 
 router.delete(
-	"/partner-cancel-order/:id",
-	verifyToken,
-	OrderController.partnerCancelOrderByID
+  "/partner-cancel-order/:id",
+  verifyToken,
+  OrderController.partnerCancelOrderByID
 );
 
 // router.post("/review-order", verifyToken, OrderController.reviewOrder);
