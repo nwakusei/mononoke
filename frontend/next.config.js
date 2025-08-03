e169f599ae9faf7f050b979459bba2fc6d42e0e4
Reinaldo Guedes <rguedes.dev@gmail.com>
2025-08-03 04:52:17 -0300

    Realizado configurações para ignorar os erros do TS por hora.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,17 +4,20 @@
 // module.exports = nextConfig;
 
 const nextConfig = {
-	// suas configurações existentes, se houver
+  // suas configurações existentes, se houver
+  typescript: {
+    ignoreBuildErrors: true, // Essa setagem não existe por padrão. Inserida para ignorar erros de tipagem
+  },
 };
 
 module.exports = {
-	...nextConfig,
-	images: {
-		...nextConfig.images,
-		domains: [
-			"localhost",
-			"backend",
-			"mononokebucket.s3.us-east-1.amazonaws.com",
-		],
-	},
+  ...nextConfig,
+  images: {
+    ...nextConfig.images,
+    domains: [
+      "localhost",
+      "backend",
+      "mononokebucket.s3.us-east-1.amazonaws.com",
+    ],
+  },
 };
