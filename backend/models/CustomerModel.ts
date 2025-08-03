2b4b4af1f638168cdb4dc87e4e6a746543f13406
Reinaldo Guedes <rguedes.dev@gmail.com>
2025-08-03 02:41:00 -0300

    Correção na tipagem do endereço.

--- backend/models/CustomerModel.ts
+++ backend/models/CustomerModel.ts
@@ -1,94 +1,103 @@
 import mainDB from "../db/mainconn.js";
 import mongoose, { Schema, model } from "mongoose";
 
+interface IAddress {
+  street?: string;
+  complement?: string;
+  neighborhood?: string;
+  city?: string;
+  state?: string;
+  postalCode?: string;
+}
+
 interface IFollowingStores {
-	storeID: string;
-	storeName: string;
+  storeID: string;
+  storeName: string;
 }
 
 // Interface tipando os dados que irão no Banco de Dados.
 interface ICustomer {
-	accountStatus: string;
-	accountType: string;
-	profileImage: string;
-	name: string;
-	nickname: string;
-	email: string;
-	password: string;
-	cpf: string;
-	address: [{}];
-	viewAdultContent: boolean;
-	followingStores: IFollowingStores[];
-	newsletters: boolean;
-	otakupayID: string;
-	resetPasswordToken?: string;
-	resetPasswordExpires?: Date;
+  accountStatus: string;
+  accountType: string;
+  profileImage: string;
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  cpf: string;
+  address: IAddress[];
+  viewAdultContent: boolean;
+  followingStores: IFollowingStores[];
+  newsletters: boolean;
+  otakupayID: string;
+  resetPasswordToken?: string;
+  resetPasswordExpires?: Date;
 }
 
 // Schema que corresponda a Interface.
 const customerSchema = new Schema<ICustomer>(
-	{
-		accountStatus: {
-			type: String,
-			required: true,
-		},
-		accountType: {
-			type: String,
-			required: true,
-		},
-		profileImage: {
-			type: String,
-		},
-		name: {
-			type: String,
-			required: true,
-		},
-		nickname: {
-			type: String,
-			unique: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-			min: 6,
-			max: 64,
-		},
-		cpf: {
-			type: String,
-			unique: true,
-		},
-		address: {
-			type: [{}],
-		},
-		viewAdultContent: {
-			type: Boolean,
-		},
-		followingStores: {
-			type: [{}],
-		},
-		newsletters: {
-			type: Boolean,
-		},
-		otakupayID: {
-			type: String,
-			ref: "OtakupayModel",
-		},
+  {
+    accountStatus: {
+      type: String,
+      required: true,
+    },
+    accountType: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 64,
+    },
+    cpf: {
+      type: String,
+      unique: true,
+    },
+    address: {
+      type: [{}],
+    },
+    viewAdultContent: {
+      type: Boolean,
+    },
+    followingStores: {
+      type: [{}],
+    },
+    newsletters: {
+      type: Boolean,
+    },
+    otakupayID: {
+      type: String,
+      ref: "OtakupayModel",
+    },
 
-		resetPasswordToken: {
-			type: String,
-			default: null,
-		},
-		resetPasswordExpires: {
-			type: Date,
-			default: null,
-		},
-	},
-	{ timestamps: true }
+    resetPasswordToken: {
+      type: String,
+      default: null,
+    },
+    resetPasswordExpires: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
 );
 
 // Criação de um Model com conexão ao banco de dados
